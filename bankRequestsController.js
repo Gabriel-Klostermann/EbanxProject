6a4fe830f46dad3c46ca7855539848e5a33dd7c5
Gabriel <souzakg.gs@gmail.com>
2020-09-10 22:34:22 -0300

    Remove internal functions that are not needed to be exposed from module.exports

--- bankRequestsController.js
+++ bankRequestsController.js
@@ -1,28 +1,27 @@
 var accounts = {};
 
-//TODO: REMOVE FUNCTIONS FROM MODULE.EXPORTS THAT ARE NOT ACCESSED FROM EXTERNAL SOURCES
+depositAmount = function (accountId, balance) {
+    accounts[accountId] += balance
+    console.log('DEPOSITED ' + balance)
+}
+createNewAccount = function (accountId, balance) {        
+    accounts[accountId] = balance
+    console.log('CREATED NEW ACCOUNT WITH ID ' + accountId)
+}
+doesAccountExist = function (accountId) {   
+    for (var key in accounts) {
+        console.log('KEY' + key)
+        if (key === accountId) {
+            return true
+        }
+    }
+    return false
+}
 
 module.exports = {
-    depositAmount: function (accountId, balance) {
-        accounts[accountId] += balance
-        console.log('DEPOSITED ' + balance)
-    },
-    createNewAccount: function (accountId, balance) {        
-        accounts[accountId] = balance
-        console.log('CREATED NEW ACCOUNT WITH ID ' + accountId)
-    },
-    doesAccountExist: function (accountId) {   
-        for (var key in accounts) {
-            console.log('KEY' + key)
-            if (key === accountId) {
-                return true
-            }
-        }
-        return false
-    },
     getBalance: function (accountId) {
         let response = {}
-        if (module.exports.doesAccountExist(accountId)) {
+        if (doesAccountExist(accountId)) {
             response.status = 200
             response.balance = accounts[accountId].toString()
             return response
@@ -34,16 +33,16 @@ module.exports = {
     },
     processEvent: function (eventData) {
         let response = {}
-        if (eventData.type === 'deposit' && module.exports.doesAccountExist(eventData.destination)) {
-            module.exports.depositAmount(eventData.destination, eventData.amount)
+        if (eventData.type === 'deposit' && doesAccountExist(eventData.destination)) {
+            depositAmount(eventData.destination, eventData.amount)
             response.status = 201
             response.json = {"destination": {"id":eventData.destination, "balance": accounts[eventData.destination].toString()}}
             return response
-        } else if (eventData.type === 'deposit' && !module.exports.doesAccountExist(eventData.destination)){
-            module.exports.createNewAccount(eventData.destination, eventData.amount)
+        } else if (eventData.type === 'deposit' && !doesAccountExist(eventData.destination)){
+            createNewAccount(eventData.destination, eventData.amount)
             response.status = 201
             response.json = {"destination": {"id":eventData.destination, "balance": eventData.amount}}
             return response
-        }
+        } 
     }
 }
